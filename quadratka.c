9945b74b0a83118fbf6bb8b550b051e386bf8aed
KirillTFK <142966225+KirillTFK@users.noreply.github.com>
2023-08-23 10:17:11 +0400

    поменял всякие неполадки, но все еще не работает

--- quadratka.c
+++ quadratka.c
@@ -7,29 +7,28 @@
 #include <math.h>
 #include <assert.h>
 
-void reader (double a, double b, double c);
+void read_coeffs (double* a, double* b, double* c);
 void quadratic_solver (const double a, const double b, const double c,
-                        double x1, double x2);
+                        double* x1, double* x2);
+void check_input (double* letter);
+int is_it_zer0 (const double num);
 
 int main (void)
 {
     double a = 0, b = 0, c = 0;
     char ch = 0;
 
-    printf ("Введите квадратное уравнение\n"
-            "В виде ax^2+bx+c=0\n");
-    printf ("Символ степени покажите так: ^\n"
-            "а в качестве переменной используйте x\n");
-
+    printf("Я ожидаю от Вас квадратное уравнение вида"
+           "ax^2+bx+c=0");
     while (1)
     {
-        printf ("Введите квадратное уравнение\n");
-        reader (a, b, c);
+        
+        read_coeffs (&a, &b, &c);
         printf ("Ваши коэффициенты:\n"
                "a = %f b = %f, c = %f\n", a, b, c);
 
         double x1 = 0, x2 = 0;
-        quadratic_solver (a, b, c, x1, x2);
+        quadratic_solver (a, b, c, &x1, &x2);
 
         printf ("Хотите продолжить? Если да, введите *\n"
                "Если хотите завершить программу введите #\n");
@@ -53,59 +52,56 @@ int main (void)
 
     return 0;
 }
-
-void reader (double a, double b, double c)
+void read_coeffs (double* a, double* b, double* c)
 {
     printf ("Введите коэффициенты\n");
     printf ("a=");
-    scanf ("%lf", &a);
+    check_input (a);
     
     printf ("\nb=");
-    scanf ("%lf", &b);
+    check_input (b);
     
     printf ("\nc=");
-    scanf ("%lf", &c);
+    check_input (c);
 
 }
 
 void quadratic_solver (const double a, const double b, const double c,
-                        double x1, double x2)
+                        double* x1, double* x2)
 {
 
-    if (fabs (a)<0.00001)
+    if (is_it_zer0(a))
     {
         printf ("Это уравнение, конечно, линейное, но я все-таки решу его\n");
-        if (b != 0)
+        if ( !(is_it_zer0(b)) )
         {
-            x1 = -b/c;
-            printf ("Одно решение:%f", x1);
+            *x1 = -c/b;
+            printf ("Одно решение:%f\n", x1);
         }
-        else if(c == 0)
+        else if ( is_it_zer0(b) && is_it_zer0(c) )
             printf ("Бесконечное число решений\n");
         else 
             printf ("Решений нет\n");
     }
 
-    int D=0;
+    double D=0;
 
-    if (a != 0)
+    if ( !(is_it_zer0(a)) )
     {
         D = b*b-4*a*c;
 
         if(D < 0)
-            printf("Решений нет\n");
+            printf ("Решений нет\n");
         
-        if (D == 0)
+        if ( is_it_zer0(D) )
         {
-            x1 = -b/(2*a);
-            assert (isfinite(x1));
-            printf ("Одно решение:%f\n", x1);
+            *x1 = -b/(2*a);
+            printf ("Ваше уравнение имеет одно решение:%f\n", x1);
         }
         if (D>0)
         {
-            assert(D>0);
-            x1 = (-b + sqrt(D))/(2*a);
-            x2 = (-b - sqrt(D)/(2*a));
+            *x1 = (-b + sqrt(D))/(2*a);
+            *x2 = (-b - sqrt(D)/(2*a));
             printf ("Ваше уравнение имеет два решения %f и %f\n", x1, x2);
         }
         
